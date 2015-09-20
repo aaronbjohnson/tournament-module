b9216739736ae0f8c6ab48f4fbc5f257b6ba4a18
Aaron <aj65461@gmail.com>
2015-09-20 17:17:21 -0400

    Add create table commands to tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,28 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE tournaments (
+    id serial primary key,
+    name text
+);
+
+CREATE TABLE members (
+    id serial primary key,
+    name text,
+    wins integer default 0,
+    losses integer default 0,
+    tournament integer references tournaments(id)
+);
+
+CREATE TABLE matches (
+    id serial primary key,
+    tournament integer references tournaments(id),
+    winner integer references members(id),
+    loser integer references members(id)
+);
